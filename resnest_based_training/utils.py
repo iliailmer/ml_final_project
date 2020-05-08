047e771464c0067f5a5bc38e352804710f6578f2
Ilia Ilmer <i.ilmer@icloud.com>
2020-05-08 16:25:49 -0400

    change of project direction

--- resnest_based_training/utils.py
+++
@@ -1,8 +0,0 @@
-def replace(network, original, replacement, name):
-    def replace_(m, name, original=original, replacement=replacement):
-        for attr_str in dir(m):
-            target_attr = getattr(m, attr_str)
-            if type(target_attr) == original:
-                setattr(m, attr_str, replacement())
-    for m in network.modules():
-        replace_(m, name)
